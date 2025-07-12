d62112bdf8513e2a054477af03ea416b73b31409
Grace <40832036+GW122910@users.noreply.github.com>
2025-07-13 00:16:29 +0100

    Added basic JavaScript form handling and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Poem shown here",
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
