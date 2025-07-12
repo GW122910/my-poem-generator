6856be3cc1f6c683eb7ad3ed9958281fbfa6a9e2
Grace <40832036+GW122910@users.noreply.github.com>
2025-07-13 00:21:37 +0100

    added poem example

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: "Poem shown here",
+    strings:
+      "Sun-kissed sands so white, Waves crash soft, a gentle song, Island dreams take flight. Palm trees sway in breeze, Ocean's breath, a salty kiss, Holiday's sweet ease.",
     autoStart: true,
     delay: 10,
     cursor: "",
