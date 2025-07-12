ab4026d084786d891efb23e07ba2d677185fbb47
Grace <40832036+GW122910@users.noreply.github.com>
2025-07-13 00:47:50 +0100

    Applied axios API integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("displaying poem");
 
   new Typewriter("#poem", {
-    strings:
-      "Sun-kissed sands so white, Waves crash soft, a gentle song, Island dreams take flight. Palm trees sway in breeze, Ocean's breath, a salty kiss, Holiday's sweet ease.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "becd8to447f4738fdfa70fbace5884f5";
+  let prompt = `Write me a short rhyming poem based on ${userInput.value}`;
+  let context =
+    "You are a poetic AI capable of writing short rhyming poems about the subject asked. Generate the poem following <p> and <br /> rules. Sign off the poem with <strong>SheCodes AI</strong>.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
